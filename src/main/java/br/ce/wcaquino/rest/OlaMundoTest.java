9a00839dc44e02b259d3f5b717c1ebd4345dd5af
kelvson <kelvsonpr@gmail.com>
2023-04-25 15:18:52 -0300

    devoConhecerMatchersHamcrest

--- src/main/java/br/ce/wcaquino/rest/OlaMundoTest.java
+++ src/main/java/br/ce/wcaquino/rest/OlaMundoTest.java
@@ -1,7 +1,14 @@
 package br.ce.wcaquino.rest;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -36,6 +43,27 @@ public class OlaMundoTest {
 				.get("http://restapi.wcaquino.me/ola")
 			.then()			//verificações
 				.statusCode(200);
+	}
+	
+	@Test
+	public void devoConhecerMatchersHamcrest() {
+		assertThat("Maria", Matchers.is("Maria")); // comparar dado
+		assertThat(128, Matchers.isA(Integer.class));
+		assertThat(128d, Matchers.isA(Double.class)); // valitar tipo de dado inteiro double string
+		assertThat(128d, Matchers.greaterThan(120d)); //Maior que
+		assertThat(128d, Matchers.lessThan(130d)); //Menor que
+		
+		List<Integer> impares = Arrays.asList(1,3,5,7,9);
+		assertThat(impares, hasSize(5)); // Validar tamanha da lista
+		assertThat(impares, contains(1,3,5,7,9)); // Contem na mesma ordem
+		assertThat(impares, containsInAnyOrder(1,3,7,9,5));// Contem em qualquer ordem
+		assertThat(impares, hasItem(1));//verificar 1 item da lista
+		assertThat(impares, hasItems(1,5));//verificar maisde um item da lista
+		
+		assertThat("Maria", is(not("João"))); // não é 
+		assertThat("Maria", not("João")); // não é
+		assertThat("Joaquina", anyOf(is("Maria"), is("Joaquina"))); // um ou outro
+		assertThat("Joaquina", allOf(startsWith("Joa"), endsWith("ina"), containsString("qui")));// começa com, termina com, e contem
 	}
 
 }
